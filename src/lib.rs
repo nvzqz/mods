5ab56b8b566de844ae0b522c170ebd9512d9214a
Nikolai Vazquez <nikvzqz@gmail.com>
2020-03-14 17:21:04 -0400

    [Docs] Add installation instructions

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,44 @@
 //! mod h;
 //! ```
 //!
+//! # Installation
+//!
+//! This crate is available [on crates.io][crate] and can be used by adding the
+//! following to your project's [`Cargo.toml`]:
+//!
+//! ```toml
+//! [dependencies]
+//! mods = "1.0.0"
+//! ```
+//!
+//! ## Minimum Supported Rust Version (MSRV)
+//!
+//! This library requires Rust 1.9.0 as the minimum version and will work with
+//! all subsequent versions.
+//!
+//! This is because  previous versions can't have `mod x;` declarations within
+//! submodules [When testing this](https://github.com/nvzqz/mods/runs/508242550)
+//! we get an error complaining that the module file is not inside the directory
+//! "src".
+//!
+//! ## Rust 2015
+//!
+//! If you're not using [Rust 2018], add this to your crate root (`main.rs` or
+//! `lib.rs`):
+//!
+//! ```rust,ignore
+//! #[macro_use]
+//! extern crate mods;
+//! ```
+//!
+//! You can then use the macro directly from anywhere:
+//!
+//! ```rust,ignore
+//! mods! {
+//!     pub puppy, kitty;
+//! }
+//! ```
+//!
 //! # Wishful Thinking
 //!
 //! It would be wonderful if we could instead have:
@@ -79,8 +117,10 @@
 //! </a>
 //!
 //! [@NikolaiVazquez]: https://twitter.com/NikolaiVazquez
-//!
-//! [`CHANGELOG.md`]: https://github.com/nvzqz/mods/blob/master/CHANGELOG.md
+//! [`Cargo.toml`]:    https://doc.rust-lang.org/cargo/reference/manifest.html
+//! [`CHANGELOG.md`]:  https://github.com/nvzqz/mods/blob/master/CHANGELOG.md
+//! [crate]:           https://crates.io/crates/mods
+//! [Rust 2018]:       https://blog.rust-lang.org/2018/12/06/Rust-1.31-and-rust-2018.html#rust-2018
 
 #![no_std]
 #![warn(missing_docs)]
